747249756ed320f14690051c9f3ec06a83054291
Antonio Polo de Alvarado <apolo@nymiz.com>
2025-04-26 10:11:29 +0200

    Batch size changed to 4

--- train.py
+++ train.py
@@ -95,8 +95,8 @@ def main():
         output_dir="./results",
         run_name="distilbert-amazon-reviews",  # Add a distinct run name
         learning_rate=2e-5,
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=8,
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=4,
         num_train_epochs=1,
         weight_decay=0.01,
         eval_strategy="steps",  # Changed from "epoch" to "steps"
